866e67425d18723523e194d5b2aad3187d0dcfa4
pgz <838070494@qq.com>
2018-06-07 16:06:10 +0800

    remove import os

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # all the imports
-import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
